8a1c6862a4877d4e55af777926a4fe681d57d9e8
Gerson <gersonbarbosa7@gmail.com>
2021-07-25 20:43:40 -0300

    Simple components test with testing-library

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,21 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import { render, waitFor } from '@testing-library/react-native';
+import FlatHeader from '../src/components/FlatHeader';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+test('should be contain text', async () => {
+  
+  const { getByText } = render(<FlatHeader />);
+  const expectText = getByText('Lista de filmes');
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  await waitFor(() => expect(expectText).toBeTruthy());
+  
+});
+
+test('should be contain credits', async () => {
+  
+  const { getByText } = render(<FlatHeader />);
+  const expectEmail = getByText('Criado por Gerson Barbosa, gersonbarbosa7@gmail.com');
+
+  await waitFor(() => expect(expectEmail).toBeTruthy());
+  
 });
